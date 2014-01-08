c1b7c1d79f45e57d45d12023b76b874327afe375
Matthieu <matthieudelaro@gmail.com>
2014-01-08 11:11:52 +0100

    Initialisation
    Copy-Paste TP1.
    Delete useless stuff.

---
+++ tp2graphes/tp2graphes/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  tp2graphes
+//
+//  Created by Matthieu on 1/8/14.
+//  Copyright (c) 2014 Matthieu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
